fd7ce16be5c0b6a45ff0b9f6c22c6355121ff86d
Ilia A. Amiri <37903573+iliaamiri@users.noreply.github.com>
2022-12-09 15:13:02 -0800

    ad

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,10 +1,11 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 import {PrismaAdapter} from "@next-auth/prisma-adapter";
-import {prisma} from "../../../prisma/index.js";
+import {prisma} from "../../../prisma";
 
 export const authOptions = {
     // Configure one or more authentication providers
+    secret: process.env.OAUTH_NEXT_SECRET,
     adapter: PrismaAdapter(prisma),
     providers: [
         GithubProvider({
