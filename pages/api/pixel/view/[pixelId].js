bb250b8851b3de5ed24e7ee8582bbbabe6270fd3
Ilia A. Amiri <37903573+iliaamiri@users.noreply.github.com>
2022-12-09 15:06:13 -0800

    fix bug

--- pages/api/pixel/view/[pixelId].js
+++ pages/api/pixel/view/[pixelId].js
@@ -1,9 +1,11 @@
-import {session} from "next-auth/core/routes";
 import {prisma} from "../../../../prisma";
+import {unstable_getServerSession} from "next-auth";
+import {authOptions} from "../../auth/[...nextauth]";
 
 export default async function handler(req, res) {
     const { pixelId } = req.query;
 
+    const session = await unstable_getServerSession(req, res, authOptions);
     if (!session || !session.user) {
         res.status(400).send({
             error: "You must be signed in to view the protected content on this page.",
